a35bed8ea5211f54bda197e435a9d84a3da790ea
Mert <merti070707@gmail.com>
2024-10-19 00:12:15 +0300

    added functionality to the encrypt and decrypt button and some quality of life features (i just want to be loved)

--- main.py
+++ main.py
@@ -2,6 +2,5 @@ from app import SecureBox
 import customtkinter as ctk
 
 if __name__ == "__main__":
-    win = ctk.CTk()
-    app = SecureBox(win)
-    win.mainloop()
+    app = SecureBox(ctk.CTk())
+    app.run()
